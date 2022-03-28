9efd39f591f797dd7371059ffeb787cf2826ab8a
Gurol Bilgin <gurolbilgin@gmail.com>
2022-03-28 06:23:10 -0600

    new files are added from the new branch

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const test = () => {
-<<<<<<< HEAD
-  return <div>master</div>;
-=======
   return <div>test</div>;
->>>>>>> f1edee6f3199859a5cf423899291adf49699ae65
 };
 
 export default test;
